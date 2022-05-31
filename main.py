fd2e6dfbbab33f634585b3b9282f9396c51c9654
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-05-31 09:22:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,13 @@
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
-    if sorted(word) == sorted(anagram):
+    remaining_anagram = anagram
+    for letter in word:
+        # print(letter)
+        if letter in remaining_anagram:
+            remaining_anagram = remaining_anagram.replace(letter, '', 1)
+            # print(remaining_anagram)
+    if remaining_anagram == "":
         return True
     else:
         return False
