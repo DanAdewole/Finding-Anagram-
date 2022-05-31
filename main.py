c3e8449709b32407be4250bdef5dc82e814d3c7d
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-05-31 09:23:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,14 @@
 # find_anagrams("hello", "check") --> False
 # find_anagrams("below", "elbow") --> True
 
-# from collections import Counter
-
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
     remaining_anagram = anagram
     for letter in word:
-        # print(letter)
         if letter in remaining_anagram:
             remaining_anagram = remaining_anagram.replace(letter, '', 1)
-            # print(remaining_anagram)
+           
     if remaining_anagram == "":
         return True
     else:
